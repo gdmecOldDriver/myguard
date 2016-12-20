8b110a2709176aa384b7c3939578af007736c85b
gdmec07150815 <wadehjd@2980.com>
2016-12-20 13:46:58 +0800

    13.42

--- app/src/main/java/edu/gdmec/s07150815/myguard/m5virusscan/dao/AntiVirusDao.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m5virusscan/dao/AntiVirusDao.java
@@ -1,7 +1,74 @@
 package edu.gdmec.s07150815.myguard.m5virusscan.dao;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 /**
  * Created by D on 2016/12/19.
  */
 public class AntiVirusDao {
+    /**检查某个MD5是否有病毒
+     * return null 表示扫描安全*/
+    private static Context context;
+    private static String dbname;
+    public AntiVirusDao(Context context){
+        this.context = context;
+        dbname = "/data/data/"+context.getPackageName()+"/files/antivirus.db";
+    }
+    public String checkVirus(String md5){
+        String desc = null;
+        //打开病毒库
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbname,null,SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor = db.rawQuery("select desc from datebale where md5=?",new String[]{md5});
+        if (cursor.moveToNext()){
+            desc = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return desc;
+    }
+
+    /**判断数据库文件是否存在*/
+    public boolean isDBExit(){
+        File file = new File(dbname);
+        return file.exists()&&file.length()>0;
+    }
+
+    /** 获取数据库版本号*/
+    public String getDBVersionNum(){
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbname,null,SQLiteDatabase.OPEN_READONLY);
+        String versionnumber = "0";
+        Cursor cursor = db.rawQuery("select subcnt from version",null);
+        if (cursor.moveToNext()){
+            versionnumber = cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return versionnumber;
+    }
+    /**更新数据库*/
+    public void  updateDBVersion(int newversion){
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbname,null,SQLiteDatabase.OPEN_READONLY);
+        String versionnumber  = "0";
+        ContentValues values = new ContentValues();
+        values.put("subcnt",newversion);
+        db.update("version",values,null,null);
+        db.close();
+    }
+    /**更新病毒库的API*/
+    public void  add(String desc,String md5){
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbname,null,SQLiteDatabase.OPEN_READONLY);
+        ContentValues values = new ContentValues();
+        values.put("md5",md5);
+        values.put("desc",desc);
+        values.put("type",6);
+        values.put("name","Android.Hark.i22hkt.a");
+        db.insert("dataable",null,values);
+        db.close();
+    }
 }
--- app/src/main/java/edu/gdmec/s07150815/myguard/m5virusscan/utils/MD5Utils.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m5virusscan/utils/MD5Utils.java
@@ -1,7 +1,40 @@
 package edu.gdmec.s07150815.myguard.m5virusscan.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+
 /**
  * Created by D on 2016/12/19.1
  */
 public class MD5Utils {
+
+    public static String getFileMd5(String path){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("md5");
+            File file= new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len=fis.read(buffer))!=-1){
+                digest.update(buffer,0,len);
+            }
+            byte[] result = digest.digest();
+            StringBuffer sb = new StringBuffer();
+            for (byte b:result){
+                int number = b&0xff;
+                String hex = Integer.toHexString(number);
+                if (hex.length()==1){
+                    sb.append("0"+hex);
+
+                }else {
+                    sb.append(hex);
+                }
+            }
+            return sb.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
