b7f9eb787c200532621f5e6b14875068ea57ed3b
gdmec07150841 <a564268706@outlook.com>
2016-12-20 22:23:19 +0800

    mdnew:37

--- app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/ContactSelectActivity.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/ContactSelectActivity.java
@@ -1,13 +1,22 @@
 package edu.gdmec.s07150815.myguard.m3communicationguard;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.List;
 
+import edu.gdmec.s07150815.myguard.R;
 import edu.gdmec.s07150815.myguard.m3communicationguard.adapter.ContactAdapter;
 import edu.gdmec.s07150815.myguard.m3communicationguard.entity.ContactInfo;
+import edu.gdmec.s07150815.myguard.m3communicationguard.utils.ContactInfoParser;
 
 /**
  *
@@ -17,9 +26,58 @@ public class ContactSelectActivity extends AppCompatActivity implements View.OnC
    private ListView mListView;
     private ContactAdapter adapter;
     private List<ContactInfo> systemContacts;
+    Handler mHandler=new Handler() {
+     public void handleMessage(android.os.Message msg){
+      switch (msg.what){
+       case 10:
+        if (systemContacts!=null){
+         adapter=new ContactAdapter(systemContacts,ContactSelectActivity.this);
+         mListView.setAdapter(adapter);
+        }
+        break;
+      }
+     };
+    };
 
-    @Override
-    public void onClick(View view) {
+ @Override
+ protected void onCreate(@Nullable Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  requestWindowFeature(Window.FEATURE_NO_TITLE);
+  setContentView(R.layout.activity_contact_select);
+  initView();
+ }
+ private void initView(){
+ findViewById(R.id.titlebar).setBackgroundColor(getResources().getColor(R.color.bright_purple));
+  ImageView mLeftImgv=findViewById(R.id.imgv_lefbtn);
+  mLeftImgv.setOnClickListener(this);
+  mLeftImgv.setImageResource(R.drawable.back);
+  mListView= (ListView) findViewById(R.id.lv_contact);
+  new Thread(){
+   public void run(){
+    systemContacts= ContactInfoParser.getSystemContact(ContactSelectActivity.this);
+    systemContacts.addAll(ContactInfoParser.getSimContacts(ContactSelectActivity.this));
+    mHandler.sendEmptyMessage(10);
+   };
+  }.start();
+  mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+   @Override
+   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    ContactInfo item= (ContactInfo) adapter.getItem(position);
+    Intent intent=new Intent();
+    intent.putExtra("phone",item.phone);
+    intent.putExtra("name",item.name);
+    setResult(0,intent);
+    finish();
+   }
+  });
+ }
 
+ @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+         case R.id.imgv_leftbtn:
+          finish();
+          break;
+        }
     }
 }
--- app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/adapter/BlackContactAdapter.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/adapter/BlackContactAdapter.java
@@ -1,7 +1,95 @@
 package edu.gdmec.s07150815.myguard.m3communicationguard.adapter;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import edu.gdmec.s07150815.myguard.R;
+import edu.gdmec.s07150815.myguard.m3communicationguard.db.dao.BlackNumberDao;
+import edu.gdmec.s07150815.myguard.m3communicationguard.entity.BlackContactInfo;
+
 /**
  * Created by 马达 on 2016/12/20.
  */
-public class BlackContactAdapter {
+public class BlackContactAdapter extends BaseAdapter{
+   private List<BlackContactInfo> contactInfos;
+    private Context context;
+    private BlackNumberDao dao;
+    private BlackContactCallBack callBack;
+    public void setCallBack(BlackContactCallBack callBack){
+        this.callBack=callBack;
+    }
+    public BlackContactAdapter(List<BlackContactInfo> systemContacts,Context context){
+        super();
+        this.contactInfos=systemContacts;
+        this.context=context;
+        dao=new BlackNumberDao(context);
+    }
+    @Override
+    public int getCount() {
+        return contactInfos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return contactInfos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder=null;
+        if (convertView==null){
+            convertView=View.inflate(context, R.layout.item_list_blackcontact,null);
+            holder=new ViewHolder();
+            holder.mNameTV= (TextView) convertView.findViewById(R.id.tv_black_name);
+            holder.mModeTV= (TextView) convertView.findViewById(R.id.tv_black_mode);
+            holder.mContactImgv=convertView.findViewById(R.id.view_black_icon);
+            holder.mDeleteView=convertView.findViewById(R.id.view_black_delete);
+            convertView.setTag(holder);
+        }else {
+            holder= (ViewHolder) convertView.getTag();
+        }
+        holder.mNameTV.setText(contactInfos.get(position).contactName+"("+contactInfos.get(position).phoneNumber+")");
+        holder.mModeTV.setText(contactInfos.get(position).getModeString(contactInfos.get(position).mode));
+        holder.mNameTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
+        holder.mModeTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
+        holder.mContactImgv.setBackgroundResource(R.drawable.brightpurple_contact_icon);
+        holder.mDeleteView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                boolean datele=dao.delete(contactInfos.get(position));
+                if (datele){
+                    contactInfos.remove(contactInfos.get(position));
+                    BlackContactAdapter.this.notifyDataSetChanged();
+                    //如果数据库中没有数据，则执行回调函数
+                    if (dao.getTotalNumber()==0){
+                        callBack.DataSizeChange();
+                    }
+                }else {
+                    Toast.makeText(context,"删除失败!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return convertView;
+    }
+    class ViewHolder{
+        TextView mNameTV;
+        TextView mModeTV;
+        View mContactImgv;
+        View mDeleteView;
+    }
+    public interface BlackContactCallBack{
+        void DataSizeChange();
+    }
 }
