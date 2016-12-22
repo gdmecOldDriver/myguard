50190734cd57cbb42975b6e30aedc7cbd2476e47
xxccmd <a564268706@outlook.com>
2016-12-22 15:47:51 +0800

    m6 done

--- app/src/main/java/edu/gdmec/s07150815/myguard/m6cleancache/entity/CacheInfo.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m6cleancache/entity/CacheInfo.java
@@ -1,8 +1,14 @@
 package edu.gdmec.s07150815.myguard.m6cleancache.entity;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by student on 16/12/22.
  */
 
 public class CacheInfo {
+    public String packagename;
+    public long cacheSize;
+    public Drawable appIcon;
+    public String appName;
 }
