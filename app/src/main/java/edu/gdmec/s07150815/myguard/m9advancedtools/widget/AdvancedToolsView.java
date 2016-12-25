026f01acddf2f58b385d01798993dfe029d5de25
hannoys <354453779@163.com>
2016-12-25 17:20:15 +0800

    m9 ys complete

--- app/src/main/java/edu/gdmec/s07150815/myguard/m9advancedtools/widget/AdvancedToolsView.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m9advancedtools/widget/AdvancedToolsView.java
@@ -1,7 +1,55 @@
 package edu.gdmec.s07150815.myguard.m9advancedtools.widget;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import edu.gdmec.s07150815.myguard.R;
+
 /**
  * Created by ys on 2016/12/22.
  */
-public class AdvancedToolsView {
+public class AdvancedToolsView extends RelativeLayout{
+    private TextView mDesriptionTV;
+    private String desc = "";
+    private Drawable drawable;
+    private ImageView mLeftImgv;
+    public AdvancedToolsView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public AdvancedToolsView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    public AdvancedToolsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //拿到属性对象的值
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.AdvancedToolsView);
+        desc = mTypedArray.getString(R.styleable.AdvancedToolsView_desc);
+        drawable = mTypedArray.getDrawable(R.styleable.AdvancedToolsView_android_src);
+        mTypedArray.recycle();
+        init(context);
+    }
+    //控件初始化
+    private void init(Context context){
+        //将资源转化成view对象显示在自己身上
+         View view = View.inflate(context,R.layout.ui_advancedtools_view,null);
+        this.addView(view);
+        mDesriptionTV = (TextView) findViewById(R.id.tv_decription);
+        mLeftImgv = (ImageView) findViewById(R.id.imgv_left);
+        mDesriptionTV.setText(desc);
+        if(drawable!=null){
+            mLeftImgv.setImageDrawable(drawable);
+        }{
+
+        }
+    }
 }
