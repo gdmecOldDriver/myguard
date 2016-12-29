a7f95c09de4f90047c0f15c1473254e4cde3c86b
xxccmd <a564268706@outlook.com>
2016-12-29 09:50:17 +0800

    29 new

--- app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/receiver/InterceptCallReceiver.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/receiver/InterceptCallReceiver.java
@@ -2,10 +2,19 @@ package edu.gdmec.s07150815.myguard.m3communicationguard.receiver;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 import edu.gdmec.s07150815.myguard.m3communicationguard.db.dao.BlackNumberDao;
 
@@ -35,7 +44,7 @@ public class InterceptCallReceiver extends BroadcastReceiver {
                         return;
                     }
                     int blackContactMode=dao.getBlackContactMode(mIncomingNumber);
-                   /*  if (blackContactMode==1||blackContactMode==3){
+                     if (blackContactMode==1||blackContactMode==3){
                         //观察（另外一个应用程序的数据库变化）呼叫记录的变化，如果呼叫记录生成了，就把呼叫记录删除
                         Uri uri=Uri.parse("content://call_log/callls");
                         context.getContentResolver().registerContentObserver(
@@ -44,9 +53,57 @@ public class InterceptCallReceiver extends BroadcastReceiver {
                                 new CallLogObserver(new Handler(),mIncomingNumber,
                                         context));
                         endCall(context);
-                    }*/
+                    }
                     break;
             }
+        }
+    }
+    private class CallLogObserver extends ContentObserver {
+        private String incomingNumber;
+        private Context context;
+
+        public CallLogObserver(Handler handler, String mIncomingNumber, Context context) {
+
+            super(handler);
+            this.incomingNumber=incomingNumber;
+            this.context=context;
+
+        }
+        //观察到数据库内容变化调用的方法
+
+        @Override
+        public void onChange(boolean selfChange) {
+            Log.i("CallLogObserver","呼叫记录数据库的内容发生变化");
+            context.getContentResolver().unregisterContentObserver(this);
+            deleteCallLog(incomingNumber,context);
+            super.onChange(selfChange);
+        }
+    }
+
+    //清除呼叫记录
+    public void deleteCallLog(String incomingNumber,Context context){
+        ContentResolver resolver=context.getContentResolver();
+        Uri uri=Uri.parse("content://call_log/calls");
+        Cursor cursor=resolver.query(uri,new String[]{ "_id" },"number=?",
+                new String[]{ incomingNumber },"_id  desc limit 1");
+        if (cursor.moveToNext()){
+            String id=cursor.getString(0);
+            resolver.delete(uri,"_id=?",new String[]{ id });
+        }
+    }
+
+    //挂断电话
+
+
+    private void endCall(Context context) {
+        try {
+            Class clazz=context.getClassLoader().loadClass(
+                    "android.os.ServiceManager");
+            Method method=clazz.getDeclaredMethod("getService",String.class);
+            IBinder iBinder= (IBinder) method.invoke(null,
+                    Context.TELEPHONY_SERVICE);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/receiver/InterceptSmsReceiver.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m3communicationguard/receiver/InterceptSmsReceiver.java
@@ -3,15 +3,36 @@ package edu.gdmec.s07150815.myguard.m3communicationguard.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.telephony.SmsMessage;
 
+import edu.gdmec.s07150815.myguard.m3communicationguard.db.dao.BlackNumberDao;
+
+//注意，修改了receive
 public class InterceptSmsReceiver extends BroadcastReceiver {
-    public InterceptSmsReceiver() {
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SharedPreferences mSP=context.getSharedPreferences("config",Context.MODE_PRIVATE);
+        boolean BlackNumStatus=mSP.getBoolean("BlackNumStatus",true);
+        if (!BlackNumStatus){
+            //黑名单拦截关闭
+            return;
+        }
+        //如果是黑名单，则终止广播
+        BlackNumberDao dao=new BlackNumberDao(context);
+        Object[] objs= (Object[]) intent.getExtras().get("pdus");
+        for (Object obj:objs){
+            SmsMessage smsMessage=SmsMessage.createFromPdu((byte[]) obj);
+            String sender=smsMessage.getOriginatingAddress();
+            String body=smsMessage.getMessageBody();
+            if (sender.startsWith("+86")){
+                sender=sender.substring(3,sender.length());
+            }
+            int mode=dao.getBlackContactMode(sender);
+            if (mode==2||mode==3){
+                //需要按揭短信，拦截广播
+                abortBroadcast();
+            }
+        }
     }
 }
