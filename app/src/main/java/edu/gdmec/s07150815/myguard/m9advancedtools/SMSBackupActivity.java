84ce94c249b47b68e13ce91e0c597422b6608923
gdmec07150815 <wadehjd@2980.com>
2016-12-29 21:54:57 +0800

    短信

--- app/src/main/java/edu/gdmec/s07150815/myguard/m9advancedtools/SMSBackupActivity.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m9advancedtools/SMSBackupActivity.java
@@ -1,14 +1,14 @@
 package edu.gdmec.s07150815.myguard.m9advancedtools;
 
-import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -17,105 +17,119 @@ import edu.gdmec.s07150815.myguard.m9advancedtools.utils.SmsBackUpUtils;
 import edu.gdmec.s07150815.myguard.m9advancedtools.utils.UIUtils;
 import edu.gdmec.s07150815.myguard.m9advancedtools.widget.MyCircleProgress;
 
-/**
- * Created by 马达 on 2016/12/27.
- */
 
-public class SMSBackupActivity extends AppCompatActivity implements View.OnClickListener{
+public class SMSBackupActivity extends AppCompatActivity implements View.OnClickListener {
+
     private MyCircleProgress mProgressButton;
-    //标识符，用来标识备份状态
-    private boolean flag=false;
+    /*标识符，用来表示备份状态的*/
+    private boolean flag = false;
     private SmsBackUpUtils smsBackUpUtils;
-    private static final int CHANGE_BUTTON_TEXT=100;
-    private Handler handler=new Handler(){
-        public void handleMessage(android.os.Message msg){
-            switch (msg.what){
+    private static final int CHANGE_BUTTON_TEXT = 100;
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
                 case CHANGE_BUTTON_TEXT:
                     mProgressButton.setText("一键备份");
                     break;
             }
-        };
+        }
+
+        ;
     };
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_smsbackup);
-        smsBackUpUtils=new SmsBackUpUtils();
-        initView();
+        smsBackUpUtils = new SmsBackUpUtils();
+        initview();
     }
 
-    private void initView() {
-        findViewById(R.id.rl_titlebar).setBackgroundColor(getResources().getColor(R.color.bright_red));
-        ImageView mLeftImgv= (ImageView) findViewById(R.id.imgv_leftbtn);
-        ((TextView)findViewById(R.id.tv_title)).setText("短信备份");
+    public void initview() {
+        findViewById(R.id.rl_titlebar).setBackgroundColor(
+                getResources().getColor(R.color.bright_red));
+        ImageView mLeftImgv = (ImageView) findViewById(R.id.imgv_leftbtn);
+        ((TextView) findViewById(R.id.tv_title)).setText("短信备份");
         mLeftImgv.setOnClickListener(this);
         mLeftImgv.setImageResource(R.drawable.back);
-        mProgressButton= (MyCircleProgress) findViewById(R.id.mcp_smsbackup);
+
+        mProgressButton = (MyCircleProgress) findViewById(R.id.mcp_smsbackup);
         mProgressButton.setOnClickListener(this);
     }
 
-    @Override
+
     protected void onDestroy() {
-        flag=false;
+        flag = false;
         smsBackUpUtils.setFlag(flag);
         super.onDestroy();
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.imgv_leftbtn:
                 finish();
                 break;
             case R.id.mcp_smsbackup:
-                if (flag){
-                    flag=false;
+                if (flag) {
+                    flag = false;
                     mProgressButton.setText("一键备份");
-                }else {
-                    flag=true;
+                } else {
+                    flag = true;
                     mProgressButton.setText("取消备份");
                 }
                 smsBackUpUtils.setFlag(flag);
-                new Thread(){
-                    public void run(){
-                        try{
-                            boolean backUpSms=smsBackUpUtils.backUpSms(SMSBackupActivity.this,new SmsBackUpUtils.BackupStatusCallback(){
-                                @Override
-                                public void onSmsBackup(int process) {
-                                    mProgressButton.setProgress(process);
-                                }
-                                @Override
-                                public void beforeSmsBackuo(int size) {
-                                    if (size<=0){
-                                        flag=false;
-                                        smsBackUpUtils.setFlag(flag);
-                                        UIUtils.showToast(SMSBackupActivity.this,"您还没有短信!");
-                                        handler.sendEmptyMessage(CHANGE_BUTTON_TEXT);
-                                    }else {
-                                        mProgressButton.setMax(size);
-                                    }
-                                }
-                            });
-                            if (backUpSms){
-                                UIUtils.showToast(SMSBackupActivity.this,"备份成功");
-                            }else {
-                                UIUtils.showToast(SMSBackupActivity.this,"备份失败");
+                new Thread() {
+                    public void run() {
+                        try {
+                            boolean backUpSms = smsBackUpUtils.backupSms(SMSBackupActivity.this, new
+                                    SmsBackUpUtils.BackupStatusCallBack() {
+
+
+
+                                        @Override
+                                        public void beforeSmsBackup(int size) {
+                                            if (size <= 0) {
+                                                flag = false;
+                                                smsBackUpUtils.setFlag(flag);
+                                                UIUtils.showToast(SMSBackupActivity.this, "您还没有短信!");
+                                                handler.sendEmptyMessage(CHANGE_BUTTON_TEXT);
+                                            } else {
+                                                mProgressButton.setMax(size);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onSmsBackup(int process) {
+                                            mProgressButton.setProgress(process);
+                                        }
+
+
+                                    });
+                            if (backUpSms) {
+                                UIUtils.showToast(SMSBackupActivity.this, "备份成功");
+                            } else {
+                                UIUtils.showToast(SMSBackupActivity.this, "备份失败");
                             }
-                        }catch (FileNotFoundException e){
+
+                        } catch (FileNotFoundException e) {
                             e.printStackTrace();
-                            UIUtils.showToast(SMSBackupActivity.this,"文件生成失败");
-                        }catch (IllegalStateException e){
+                            UIUtils.showToast(SMSBackupActivity.this, "文件生成失败");
+                        } catch (IllegalStateException e) {
                             e.printStackTrace();
-                            UIUtils.showToast(SMSBackupActivity.this,"SD卡不可用或SD卡内存不足");
-                        }catch (IOException e){
+                            UIUtils.showToast(SMSBackupActivity.this, "SD卡不可用或SD卡内存不足");
+                        } catch (IOException e) {
                             e.printStackTrace();
-                            UIUtils.showToast(SMSBackupActivity.this,"读写错误");
+                            UIUtils.showToast(SMSBackupActivity.this, "读写错误");
                         }
-                    };
+                    }
+
+                    ;
                 }.start();
                 break;
         }
     }
 }
+
+
--- app/src/main/java/edu/gdmec/s07150815/myguard/m9advancedtools/utils/SmsBackUpUtils.java
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m9advancedtools/utils/SmsBackUpUtils.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 import android.util.Xml;
 
 import org.xmlpull.v1.XmlSerializer;
@@ -15,77 +16,104 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * Created by ys on 2016/12/22.
+ * Created by weiruibo on 12/23/16.
  */
-//短信的工具类 提供短信备份api
 
+/**
+ * 短信的工具类，提供短信备份API
+ */
 public class SmsBackUpUtils {
-    //定义一个接口，用作回调
-    public interface  BackupStatusCallback{
-        //在短信备份之前调用的方法
-        //size 总的短信个数
-        public void beforeSmsBackuo(int size);
-            //当sms短信备份过程中调用的方法
-        //process 当前的进度
+    /**
+     * 定义的一个接口，用作回调
+     */
+
+    public interface BackupStatusCallBack {
+
+        /**
+         * 在短信备份之前调用的方法
+         *
+         * @param size 总的短信的个数;
+         */
+        public void beforeSmsBackup(int size);
+
+        /**
+         * 当sms短信备份过程中调用的方法
+         *
+         * @param process 当期的进度
+         */
         public void onSmsBackup(int process);
     }
-    private boolean flag=true;
-    public void setFlag(boolean flag){
-        this.flag=flag;
+
+    private boolean flag = true;
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
     }
-    //备份短信
-    //context 上下文
-    //callback  接口
-    //return
-    //throws FlieNotFoundException
-    public boolean backUpSms(Context context,BackupStatusCallback callback)throws FileNotFoundException,IllegalStateException,IOException{
-        XmlSerializer serializer= Xml.newSerializer();
-        File sdDir= Environment.getExternalStorageDirectory();
-        long freesize=sdDir.getFreeSpace();
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)&&freesize>10241*10241){
-            File file=new File(Environment.getExternalStorageDirectory(),"backup.xml");
-            FileOutputStream os=new FileOutputStream(file);
+
+    /**
+     * 备份短信
+     *
+     * @param context  上下文
+     * @param callback 接口
+     * @return
+     * @throws FileNotFoundException
+     * @throws IllegalStateException
+     * @throws IOException
+     */
+    public boolean backupSms(Context context, BackupStatusCallBack callback)
+            throws FileNotFoundException, IllegalStateException, IOException {
+        XmlSerializer serializer = Xml.newSerializer();
+        File sdDir = Environment.getExternalStorageDirectory();
+        Log.d("SmsBackUpUtils", "内存卡:" + sdDir.toString());
+        long freesize = sdDir.getFreeSpace();
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED) && freesize > 1024l * 1024l) {
+            Log.d("SmsBackUpUtils", "内存卡已装载");
+            File file = new File(Environment.getExternalStorageDirectory(), "backup.xml");
+
+            FileOutputStream os = new FileOutputStream(file);
             //初始化xml文件的序列化器
-            serializer.setOutput(os,"utf-8");
+            serializer.setOutput(os, "utf-8");
             //写xml文件的头
-            serializer.startDocument("utf-8",true);
+            serializer.startDocument("utf-8", true);
             //写根节点
-            ContentResolver resolver=context.getContentResolver();
-            Uri uri=Uri.parse("content://sms/");
-           /* Cursor cursor=resolver.query(uri,new String[]{"address","body","type","date"},null,null,null);*/
-            Cursor cursor = resolver.query(uri, new String[] {"address","body","type","date"},null,null,null);
+            ContentResolver resolver = context.getContentResolver();
+            Uri uri = Uri.parse("content://sms/");
+            Cursor cursor = resolver.query(uri, new String[]{
+                    "address", "body", "type", "date"}, null, null, null);
             //得到总的条目的个数
-            int size=cursor.getCount();
+            int size = cursor.getCount();
+            Log.d("SmsBackUpUtils", "短信总个数" + size);
             //设置进度的总大小
-            callback.beforeSmsBackuo(size);
-            serializer.startTag(null,"smss");
-            serializer.attribute(null,"size",String.valueOf(size));
-            int process=0;
-            while (cursor.moveToNext()&flag){
-                serializer.startTag(null,"sms");
-                serializer.startTag(null,"body");
+            callback.beforeSmsBackup(size);
+            serializer.startTag(null, "smss");
+            serializer.attribute(null, "size", String.valueOf(size));
+            int process = 0;
+            while (cursor.moveToNext() & flag) {
+                serializer.startTag(null, "sms");
+                serializer.startTag(null, "body");
                 //可能会有乱码问题需要处理，如果出现乱码会导致备份失败
-                try{
-                    String bodyencpyt=Crypto.encrypt("123",cursor.getString(1));
+                try {
+                    String bodyencpyt = Crypto.encrypt("123", cursor.getString(1));
                     serializer.text(bodyencpyt);
-                }catch (Exception el){
-                    el.printStackTrace();
-                    serializer.text("短信读取失败");
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                    serializer.text("读取短信失败");
                 }
-                serializer.endTag(null,"body");
-                serializer.startTag(null,"address");
+                serializer.endTag(null, "body");
+                serializer.startTag(null, "address");
                 serializer.text(cursor.getString(0));
-                serializer.endTag(null,"address");
-                serializer.startTag(null,"type");
+                serializer.endTag(null, "address");
+                serializer.startTag(null, "type");
                 serializer.text(cursor.getString(2));
-                serializer.endTag(null,"type");
-                serializer.startTag(null,"date");
+                serializer.endTag(null, "type");
+                serializer.startTag(null, "date");
                 serializer.text(cursor.getString(3));
-                serializer.endTag(null,"date");
-                serializer.startTag(null,"sms");
+                serializer.endTag(null, "date");
+                serializer.endTag(null, "sms");
                 try {
                     Thread.sleep(600);
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 //设置进度条对话框的进度
@@ -93,13 +121,13 @@ public class SmsBackUpUtils {
                 callback.onSmsBackup(process);
             }
             cursor.close();
-            serializer.endTag(null,"smss");
+            serializer.endTag(null, "smss");
             serializer.endDocument();
             os.flush();
             os.close();
             return flag;
-        }else {
-            throw  new IllegalStateException("sd卡不存在或者空间不足");
+        } else {
+            throw new IllegalStateException("sd卡不存在或者空间不足");
         }
     }
 }
